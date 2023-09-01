c3824ccc69b69eff1bad51c0d82a7a7bc1a98f9d
earlyway <82577181+earlyway@users.noreply.github.com>
2023-09-01 13:58:15 +0900

    tiny object Control_UI 230901
    func_ 이름을 가진 vset 들만 combobox에 리스트업하기
    --------------
    구현 성공 후 커밋

--- testUIUX_HD_EY_plugin01.py
+++ testUIUX_HD_EY_plugin01.py
@@ -19,49 +19,71 @@ except ImportError:
 
 import uiTools
 
-#UI파일 연결. 단, UI파일은 Python 코드 파일과 같은 디렉토리에 위치해야한다.
-vrTOC_form, vrTOC_base = uiTools.loadUiType("testUIUX_HD_EY_plugin01.ui")
+listToLoad = False
+global comboBox_vset_list
+comboBox_vset_list = []
 
-#화면을 띄우는데 사용되는 Class 선언
+
+vrTOC_form, vrTOC_base = uiTools.loadUiType("010.ui")
+
+
 class vrWindowClass(vrTOC_form, vrTOC_base):
     def __init__(self, parent=None) :
         #Setup and connect the plugins UI.
         super(vrWindowClass, self).__init__(parent)
+        parent.layout().addWidget(self)
         self.parent = parent
         self.setupUi(self)
         
-        # 버튼 객체가 클릭되면 print 메서드 출력
-        self.btn_1.clicked.connect(self.button1Function) 
-        self.btn_2.clicked.connect(self.button2Function)
-
-    def button1Function(self) :
-        global comboBox_vset_list
-        comboBox_vset_list = []
+        self.ui_pushbutton_load.clicked.connect(self.loadFunction) 
+        self.ui_pushbutton_reset.clicked.connect(self.resetFunction)
         
-        print("btn_1 Clicked")
-        vrVariants.selectVariantSet("func_creditCard") # func_creditCard vset을 선택해서 실행시킴
-        vred_vset_list1 = vrVariantSets.getVariantSets() # 현재 오픈된 vred 파일에 있는 vset 들을 모두 가져와 list 형태로 리턴함
+        
+        
+        self.syncComboBox()
+        
+        self.vset_select_combo_box1.clicked.connect(self.cbBox1)
+        self.vset_select_combo_box2.clicked.connect(self.cbBox2)
+        '''
+        self.vset_select_combo_box1_radio_on.clicked.connect(self.radioBox1)
+        self.vset_select_combo_box1_radio_off.clicked.connect(self.radioBox1)
+        
+        self.vset_select_combo_box2_radio_on.clicked.connect(self.radioBox2)
+        self.vset_select_combo_box2_radio_off.clicked.connect(self.radioBox2)
+'''
+
+    def loadFunction(self) :
+        self.vset_select_combo_box1.clear() #초기화
+        self.vset_select_combo_box2.clear() #초기화
+        comboBox_vset_list = [] #load 버튼을 누를때마다 combobox list를 초기화. 이걸 하지않으면 list가 쌓임.
+        
+        vrVariants.selectVariantSet("func_creditCard")
+        vred_vset_list1 = vrVariantSets.getVariantSets()
         print(vred_vset_list1)
         
-        for element1 in vred_vset_list1 : # 가져온 vset lists 중에서
-            if "func_" in element1:         # func_ 문자열을 가진 원소만 가져와
-                comboBox_vset_list.append(element1)  # combobox에 들어갈 원소로 list up
+        for element1 in vred_vset_list1 : 
+            if "func_" in element1:         
+                comboBox_vset_list.append(element1)  
                 
-        print(comboBox_vset_list)
-        '''
-        if "func_" in vred_vset_list1 : #comboBox에 들어갈 원소 func_만 찾아내서 새로운 list를 만듦
-            comboBox_vset_list.append()
-        print("comboBox_vset_list")
-'''
-    def button2Function(self) :
-        print("btn_2 Clicked")
-
-'''
-if __name__ == "__main__" :
-    app = QApplication(sys.argv)
-    myWindow = vrWindowClass() 
-    myWindow.show()
-    app.exec_()
+        print("comboBox vset list = " + str(comboBox_vset_list))
+        
+        self.vset_select_combo_box1.addItems(comboBox_vset_list)
+        self.vset_select_combo_box2.addItems(comboBox_vset_list)
+        
+    def resetFunction(self) :
+        print("reset btn Clicked")
+        
+    def syncComboBox(self) :
+        for i in range(0, self.comboBox_vset_list.count()):
+            print("sync "+ i)
+            '''
+    def radioBox1(self):
+        if self.vset_select_combo_box1_radio_on.isChecked() : print("box1 on checked")
+        elif self.vset_select_combo_box1_radio_off.isChecked() : print("box1 off checked")
+        
+    def radioBox2(self):
+        if self.vset_select_combo_box2_radio_on.isChecked() : print("box2 on checked")
+        elif self.vset_select_combo_box2_radio_off.isChecked() : print("box2 off checked")
 '''
 
 if not importError:
