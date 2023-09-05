bb45863a48018fbc54cf4625cf4de367202e19cd
earlyway <82577181+earlyway@users.noreply.github.com>
2023-09-05 12:16:27 +0900

    tiny Object Control_UI 230905
    load 버튼을 누르기 전, 사전 작업이 필요함. 무엇이냐면 오브젝트를 특정 위치로 가져다놓고 hide 시켜놓아야함.
    load btn -> vred vset에서 func_ 이름만 가진 vset만 가져와서 콤보박스에 list up.
    리스트중, 하나를 선택하면 ComboBox1Click 메서드에서 현재 콤보박스 이름과 비교해 vred vset 을 실행.
    사실상 radio button 은 오브젝트 on/off만 하는 것이고 콤보박스의 클릭으로 func_ 를 실행함.
     reset 버튼을 누르면 모든 ui 리셋, vred 내의 const, loop, unused material 들도 삭제.

--- testUIUX_HD_EY_plugin01.py
+++ testUIUX_HD_EY_plugin01.py
@@ -1,4 +1,3 @@
-
 from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import Signal,Slot, QThread,QTimer
 
@@ -13,6 +12,8 @@ try:
     import vrVariantSets
     import vrVariants
     import time
+    
+
 except ImportError:
     importError = True
     pass
@@ -20,14 +21,16 @@ except ImportError:
 import uiTools
 
 listToLoad = False
-global comboBox_vset_list
-comboBox_vset_list = []
 
 
 vrTOC_form, vrTOC_base = uiTools.loadUiType("010.ui")
 
 
 class vrWindowClass(vrTOC_form, vrTOC_base):
+    
+    global comboBox_vset_list
+    comboBox_vset_list = []
+    
     def __init__(self, parent=None) :
         #Setup and connect the plugins UI.
         super(vrWindowClass, self).__init__(parent)
@@ -38,14 +41,17 @@ class vrWindowClass(vrTOC_form, vrTOC_base):
         self.ui_pushbutton_load.clicked.connect(self.loadFunction) 
         self.ui_pushbutton_reset.clicked.connect(self.resetFunction)
         
-        self.vset_select_combo_box1_radio_on.toggled.connect(self.radioBox1)
+        self.vset_select_combo_box1.currentIndexChanged.connect(self.ComboBox1Click)
+        self.vset_select_combo_box2.currentIndexChanged.connect(self.ComboBox2Click)
         
+        self.vset_select_combo_box1_radio_on.toggled.connect(self.radioBox1)
         self.vset_select_combo_box2_radio_on.toggled.connect(self.radioBox2)
 
 
     def loadFunction(self) :
         self.vset_select_combo_box1.clear() #초기화
         self.vset_select_combo_box2.clear() #초기화
+        global comboBox_vset_list
         comboBox_vset_list = [] #load 버튼을 누를때마다 combobox list를 초기화. 이걸 하지않으면 list가 쌓임.
         
         
@@ -61,41 +67,85 @@ class vrWindowClass(vrTOC_form, vrTOC_base):
         
         self.vset_select_combo_box1.addItems(comboBox_vset_list)
         self.vset_select_combo_box2.addItems(comboBox_vset_list)
-        self.vset_select_combo_box1.setCurrentIndex(0)
+        #self.vset_select_combo_box1.setCurrentIndex(0) #0905
         self.vset_select_combo_box2.setCurrentIndex(1)
-        
         
         
     def resetFunction(self) :
         print("reset btn Clicked")
+        self.vset_select_combo_box1.clear() #초기화
+        self.vset_select_combo_box2.clear() #초기화
         
-    def syncComboBox(self) :
-        for i in range(0, self.comboBox_vset_list.count()):
-            print("sync "+ i)
+        self.vset_select_combo_box1_radio_off.checked() #0905 radio button 디폴트값인 off로 지정.
+        self.vset_select_combo_box2_radio_off.checked()
+        
+        pb2 = vrNodePtr.findNode("powerbank2") #0905 오브젝트를 특정위치로 리셋
+        pb2.setWorldTranslation(1250, 1575, 1200)#0905
+        pb2.setRotation(0,0,-180)#0905
+        
+        tbr = vrNodePtr.findNode("moving_Tumbler1")
+        tbr.setWorldTranslation(1340, 1600, 1000)
+        tbr.setRotation(0,0,0)
+        
+        ccb = vrNodePtr.findNode("credit_card_blue")
+        ccb.setWorldTranslation(1500, 1650, 1000)
+        ccb.setRotation(0,0,-180)
+
+        vrVariants.selectVariantSet("vset_reset_btn") #0905 vred의 vset 호출. 여기엔 constraint 삭제, loop 삭제, 사용하지 않는 material 삭제, 모든 오브젝트를 hide.
+        
+
+    def ComboBox1Click(self) : # combobox 를 클릭해 리스트중 하나를 클릭하면 클릭된 리스트 이름을 비교.
+        print("combobox1 clicked")
+        if self.vset_select_combo_box1.currentText() == comboBox_vset_list[0] :
+            print("create power bank model")
+            
+        elif self.vset_select_combo_box1.currentText() == comboBox_vset_list[1] :
+            print("create func_tumbler model")
+            
+        elif self.vset_select_combo_box1.currentText() == comboBox_vset_list[2] :
+            print("create func_creditCard model")
+            
+            
+    def ComboBox2Click(self) :
+        print("combobox2 clicked")
+        
+        if self.vset_select_combo_box2.currentText() == comboBox_vset_list[0] :
+            print("create power bank model")
+            
+        elif self.vset_select_combo_box2.currentText() == comboBox_vset_list[1] :
+            print("create func_tumbler model")
+            
+        elif self.vset_select_combo_box2.currentText() == comboBox_vset_list[2] :
+            print("create func_creditCard model")
+            
             
     def radioBox1(self):
         if self.vset_select_combo_box1_radio_on.isChecked() : 
             print("RB1 on checked")
-            '''
-            rightnowOnVset = vset_select_combo_box1.currentText()
+            
+            rightnowOnVset = self.vset_select_combo_box1.currentText()
             print("rightnowOn" + str(rightnowOnVset))
-            RBtempVar1 = rightnowOnVset.find(rightnowOnVset, 5)
-            vrVariantSets.selectVariantSet("func"+ "_" + RBtempVar1)
-            '''
+            testA = vrVariants.selectVariantSet("_" + rightnowOnVset + "show")#0905 여기가 작동하지 않는중.. testA print를 하면 None 값이 출력되는 상황.
+            print(testA)
+            
         else : 
             print("RB1 off checked")
-            '''
-            rightnowOffVset = vset_select_combo_box1.currentText()
-            print("rightnowOff" + str(rightnowOffVset))
-            RBtempVar2 = rightnowOffVset.find(rightnowOffVset, 5)
-            vrVariantSets.selectVariantSet("_" + RBtempVar2)
-            '''
+            
+            rightnowOffVset = self.vset_select_combo_box1.currentText()
+            testB = vrVariants.selectVariantSet("_" + rightnowOffVset + "hide")#0905
+            print(testB)
         
     def radioBox2(self):
         if self.vset_select_combo_box2_radio_on.isChecked() : 
             print("RB2 on checked")
-        else : 
+            
+            rightnowOnVset2 = self.vset_select_combo_box2.currentText()
+            vrVariants.selectVariantSet("_" + rightnowOnVset2 + "show")#0905
+        elif self.vset_select_combo_box2_radio_off.isChecked() : 
             print("RB2 off checked")
+            
+            rightnowOffVset2 = self.vset_select_combo_box1.currentText()
+            vrVariants.selectVariantSet("_" + rightnowOffVset2 + "hide")#0905
 
 
 if not importError:
